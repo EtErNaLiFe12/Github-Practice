d6fcd7da592b8cd20b1c2d10e77c57711dad5a0b
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-04-30 15:56:13 +0900

    Parcel-bundler study 파일

---
+++ Parcel-bundler/.postcssrc.js
@@ -0,0 +1,23 @@
+// ESM (ECHMA SCRIPT MODULE)
+// CommonJS
+
+//import autoprefixer from 'autoprefixer'
+
+// const autoprefixer = require('autoprefixer') // nodeJS 환경에서의 import
+
+// export { plugins: [ autoprefixer ] }
+
+//module.exports = { // nodeJS 환경에서의 export
+//  plugins: [
+//    autoprefixer
+//  ]
+//}
+
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
+
+
